2e0ea63766356725fd64136398c42b049236ec40
anaines14 <anaiobarros2014@gmail.com>
2020-03-25 22:41:32 +0000

    Textures applied to MyUnitCubeQuad

--- TPs/TP4 - Application of Textures/MyUnitCubeQuad.js
+++ TPs/TP4 - Application of Textures/MyUnitCubeQuad.js
@@ -14,44 +14,75 @@ class MyUnitCubeQuad extends CGFobject {
         this.face5 = new MyQuad(this.scene);
         this.face6 = new MyQuad(this.scene);
 
+        // Side material
+        this.sideMaterial = new CGFappearance(this.scene);
+        this.sideMaterial.setAmbient(0.1, 0.1, 0.1, 1);
+        this.sideMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.sideMaterial.setSpecular(0.1, 0.1, 0.1, 1);
+        this.sideMaterial.setShininess(10.0);
+        this.sideMaterial.loadTexture('images/mineSide.png');
+        this.sideMaterial.setTextureWrap('REPEAT', 'REPEAT');
+
+        // Up material
+        this.upMaterial = new CGFappearance(this.scene);
+        this.upMaterial.setAmbient(0.1, 0.1, 0.1, 1);
+        this.upMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.upMaterial.setSpecular(0.1, 0.1, 0.1, 1);
+        this.upMaterial.setShininess(10.0);
+        this.upMaterial.loadTexture('images/mineTop.png');
+        this.upMaterial.setTextureWrap('REPEAT', 'REPEAT');
+
+        // Down material
+        this.downMaterial = new CGFappearance(this.scene);
+        this.downMaterial.setAmbient(0.1, 0.1, 0.1, 1);
+        this.downMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.downMaterial.setSpecular(0.1, 0.1, 0.1, 1);
+        this.downMaterial.setShininess(10.0);
+        this.downMaterial.loadTexture('images/mineBottom.png');
+        this.downMaterial.setTextureWrap('REPEAT', 'REPEAT');
+
     }
     display(){
         //Back
         this.scene.pushMatrix();
         this.scene.rotate(Math.PI,0, 1, 0);
+        this.sideMaterial.apply();
         this.face1.display();
-        this.scene.popMatrix();
-
-        //Front
-        this.scene.pushMatrix();
-        this.scene.translate(0,0,1);
-        this.face2.display();
-        this.scene.popMatrix();
-
-        //Down
-        this.scene.pushMatrix();
-        this.scene.rotate( Math.PI / 2, 1, 0, 0);
-        this.scene.translate(0, 0.5, 0.5)
-        this.face3.display();
         this.scene.popMatrix();
 
         //Right
         this.scene.pushMatrix();
         this.scene.rotate( Math.PI / 2, 0, 1, 0);
         this.scene.translate(-0.5, 0, 0.5)
-        this.face4.display();
+        this.face2.display();
         this.scene.popMatrix();
 
         //Left
         this.scene.pushMatrix();
         this.scene.rotate( -Math.PI / 2, 0, 1, 0);
         this.scene.translate(0.5, 0, 0.5)
-        this.face5.display();
+        this.face3.display();
         this.scene.popMatrix();
 
+        //Front
+        this.scene.pushMatrix();
+        this.scene.translate(0,0,1);
+        this.face4.display();
+        this.scene.popMatrix();
+
+        //Up
         this.scene.pushMatrix();
         this.scene.rotate( -Math.PI / 2, 1, 0, 0);
         this.scene.translate(0, -0.5, 0.5)
+        this.upMaterial.apply();
+        this.face5.display();
+        this.scene.popMatrix();
+
+        //Down
+        this.scene.pushMatrix();
+        this.scene.rotate( Math.PI / 2, 1, 0, 0);
+        this.scene.translate(0, 0.5, 0.5)
+        this.downMaterial.apply();
         this.face6.display();
         this.scene.popMatrix();
     }
