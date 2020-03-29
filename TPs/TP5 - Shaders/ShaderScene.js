96fecc7363fd1e11bb510b48e8e995660b128465
anaines14 <anaiobarros2014@gmail.com>
2020-03-29 19:17:08 +0100

    Finished ex 1.2 and 1.3

--- TPs/TP5 - Shaders/ShaderScene.js
+++ TPs/TP5 - Shaders/ShaderScene.js
@@ -69,7 +69,8 @@ class ShaderScene extends CGFscene {
 			new CGFshader(this.gl, "shaders/texture3anim.vert", "shaders/texture3anim.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/sepia.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/convolution.frag"),
-			new CGFshader(this.gl, "shaders/exercise1.vert", "shaders/exercise1.frag")
+			new CGFshader(this.gl, "shaders/exercise1.vert", "shaders/exercise1.frag"),
+			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/exercise3.frag")
 		];
 
 		// additional texture will have to be bound to texture unit 1 later, when using the shader, with "this.texture2.bind(1);"
@@ -91,7 +92,8 @@ class ShaderScene extends CGFscene {
 			'Animation example': 6,
 			'Sepia': 7,
 			'Convolution': 8,
-			'Exercise1' : 9
+			'Exercise1' : 9,
+			'Exercise3' : 10
 		};
 
 		// shader code panels references
