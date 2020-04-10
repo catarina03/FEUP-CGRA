59460067258b2ce520a732f8ab47b42491bb0e66
catarina03 <up201806610@fe.up.pt>
2020-04-10 23:12:33 +0100

    Variable number of slices done

--- TPs/proj/MyCylinder.js
+++ TPs/proj/MyCylinder.js
@@ -4,53 +4,53 @@
  * @param scene - Reference to MyScene object
  */
 class MyCylinder extends CGFobject {
-    constructor(scene) {
+    constructor(scene, slices) {
         super(scene);
-        this.initBuffers();
+        this.initBuffers(slices);
     }
 
-    initBuffers() {
+    initBuffers(slices) {
         
         // Vertices
         this.vertices = [];
-        var ang = 360/6;
+        var ang = 360/slices;
         ang = ang * Math.PI / 180;
 
-        for (var i = 0; i < 6; i++) 
+        for (var i = 0; i < slices; i++) 
             this.vertices.push(Math.cos(i*ang), 0,Math.sin(i*ang));
         this.vertices.push(1, 0, 0);   //Repetir primeiros vertices para a textura
         
-        for (var i = 0; i < 6; i++) 
+        for (var i = 0; i < slices; i++) 
             this.vertices.push(Math.cos(i*ang), 1, Math.sin(i*ang));
         this.vertices.push(1, 1, 0);  //Repetir primeiros vertices para a textura
 
         // Indices
         this.indices = [];
 
-        for (var i = 0; i < 6; i++) 
-            this.indices.push(i, i + 7, i + 8,
-                              i + 8, i + 1, i);
+        for (var i = 0; i < slices; i++) 
+            this.indices.push(i, i + slices+1, i + slices+2,
+                              i + slices+2, i + 1, i);
         
         // Normals
         this.normals = [];
 
-        for (var i = 0; i < 6; i++) 
+        for (var i = 0; i < slices; i++) 
             this.normals.push(Math.cos(i*ang), 0, Math.sin(i*ang));
         this.normals.push(1, 0, 0); 
         
-        for (var i = 0; i < 6; i++)
+        for (var i = 0; i < slices; i++)
             this.normals.push(Math.cos(i*ang), 0, Math.sin(i*ang));
         this.normals.push(1, 0, 0);
 
         // Tex
-        var aux = 1/6;
+        var aux = 1/slices;
 
         this.texCoords = [];
 
-        for (var i = 0; i < 6; i++)
+        for (var i = 0; i < slices; i++)
             this.texCoords.push(1-i*aux, 1);
         this.texCoords.push(0, 1);
-        for (var i = 0; i < 6; i++)
+        for (var i = 0; i < slices; i++)
             this.texCoords.push(1-i*aux, 0);
         this.texCoords.push(0, 0);
 
