8f7a08d667dd5c571024f612a10d9e85235b212d
catarina03 <up201806610@fe.up.pt>
2020-05-15 17:16:03 +0100

    Flag with the same movement as airship

--- proj/MyVehicle.js
+++ proj/MyVehicle.js
@@ -88,7 +88,6 @@ class MyVehicle extends CGFobject {
 		this.scene.pushMatrix();
 		this.scene.translate(0, 10, 0);
 		//this.scene.scale(this.scene.scaleFactor, this.scene.scaleFactor, this.scene.scaleFactor);
-		this.scene.scale(3, 3, 3);
 		this.scene.translate(this.pos[0], 0, this.pos[2]);
 		this.scene.rotate(this.ang*Math.PI /180, 0, 1, 0);
 		this.scene.scale(2,2,2);
@@ -97,8 +96,8 @@ class MyVehicle extends CGFobject {
 		this.scene.popMatrix();
 
 		this.scene.pushMatrix();
-		this.scene.translate(0, 10, 0);
-		this.scene.translate(this.pos[0]  - 1.8, 0, this.pos[2] - 1.8);
+		//this.scene.translate(0, 10, 0);
+		this.scene.translate(this.pos[0] + 5, 15, this.pos[2]);
 		this.scene.rotate(this.ang*Math.PI /180 + Math.PI/2, 0, 1, 0); //??
 		this.scene.scale(2, 1, 1); //Flag Shape
 		this.flag.display();
