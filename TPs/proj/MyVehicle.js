2047c12f71e0f87bc397a08b3bcc46c283785671
anaines14 <anaiobarros2014@gmail.com>
2020-05-15 14:19:12 +0100

    Fixed movement too big

--- TPs/proj/MyVehicle.js
+++ TPs/proj/MyVehicle.js
@@ -87,15 +87,15 @@ class MyVehicle extends CGFobject {
 	{
 		this.scene.pushMatrix();
 		this.scene.translate(0, 10, 0);
-		this.scene.scale(3, 3, 3);
 		this.scene.translate(this.pos[0], 0, this.pos[2]);
 		this.scene.rotate(this.ang*Math.PI /180, 0, 1, 0);
+		this.scene.scale(2,2,2);
+
 		this.airship.display();
 		this.scene.popMatrix();
 
 		this.scene.pushMatrix();
 		this.scene.translate(0, 10, 0);
-		this.scene.scale(3, 3, 3);
 		this.scene.translate(this.pos[0], 0, this.pos[2] - 1.8);
 		this.scene.rotate(this.ang*Math.PI /180 + Math.PI/2, 0, 1, 0); //??
 		this.flag.display();
